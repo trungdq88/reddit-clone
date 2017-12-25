ed6da0d956da03957bd2d67b835849e781171a3f
Trung Dinh Quang <trungdq88@gmail.com>
2017-12-25 16:59:38 +0700

    Show Submit button & List item.

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, Alert } from 'react-native';
+import LinkItem from './LinkItem.js';
 
 export default class App extends React.Component {
+  state = {
+    data: [{ value: 'A' }, { value: 'B' }],
+  };
+
+  _onPressItem = item => Alert.alert('Hello ' + item);
+
+  _renderItem = ({ item }) => (
+    <LinkItem key={item.value} {...item} onPressItem={this._onPressItem} />
+  );
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <View sytle={{ flex: 1 }}>
+          <View style={{ padding: 20 }}>
+            <Button
+              title="+ Submit Link"
+              accessibilityLabel="Submit Link"
+              onPress={e => {
+                this.setState({
+                  data: this.state.data.concat({ value: Math.random() }),
+                });
+              }}
+            />
+          </View>
+        </View>
+        <View style={{ flex: 9, width: '100%' }}>
+          <FlatList data={this.state.data} renderItem={this._renderItem} />
+        </View>
       </View>
     );
   }
@@ -14,6 +40,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 25,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ LinkItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import T from 'prop-types';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default class LinkItem extends React.Component {
+  static propTypes = {
+    value: T.any,
+    onPressItem: T.func,
+  };
+
+  _onPressItem = () =>
+    this.props.onPressItem && this.props.onPressItem(this.props.value);
+
+  render() {
+    const { value, onPressItem } = this.props;
+    return (
+      <TouchableOpacity onPress={this._onPressItem}>
+        <View style={{ backgroundColor: '#eee', padding: 20 }}>
+          <Text>{value}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
